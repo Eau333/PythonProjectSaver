479f9b392d1fcb7922b53239b5741aa6ff24b6cc
Mauricio Diaz <mauriciod@gmail.com>
2023-08-18 00:35:22 -0700

    worked a bit on the choose cpu screen

--- uno/gfx_examples.py
+++ uno/gfx_examples.py
@@ -1,6 +1,10 @@
 import uno_gfx_api
+
+
 def run_examples():
     print('Running test examples.')
     uno_gfx_api.dependency_test()
-    uno_gfx_api.game_setup()
+    uno_gfx = uno_gfx_api.unogfx()
+    uno_gfx.choose_num_players('Welcome to PyUno!')
+    uno_gfx.update_window()
 
--- uno/uno_gfx_api.py
+++ uno/uno_gfx_api.py
@@ -1,26 +1,63 @@
 import pyglet
+import random
+
 def dependency_test():
     print('API successfully invoked.')
 
-def game_setup():
-    window = pyglet.window.Window()
-    window.set_size(1500,1000)
-    label = pyglet.text.Label('Hello, world',
-                              font_name='Times New Roman',
-                              font_size=36,
-                              x=window.width // 2, y=window.height // 2,
-                              anchor_x='center', anchor_y='center')
-    @window.event
-    def on_draw():
-        window.clear()
-        label.draw()
-        pyglet.app.exit()
 
-    window.switch_to()
-    window.dispatch_event('on_draw')
-    window.dispatch_events()
-    window.flip()
+class unogfx():
+    def __init__(self):
+        self.window = pyglet.window.Window()
+        self.window.set_size(1500, 1000)
+        pyglet.gl.glClearColor(255, 255, 255, 1.0)
+        self.labels = []
+        self.shapes = []
 
-#    pyglet.app.run()
+    def update_window(self):
+        @self.window.event
+        def on_draw():
+            self.window.clear()
+            for shape in self.shapes:
+                shape.draw()
+            for label in self.labels:
+                label.draw()
+            # pyglet.app.exit()
 
+        self.window.switch_to()
+        self.window.dispatch_event('on_draw')
+        self.window.dispatch_events()
+        self.window.flip()
 
+    class clickable_circle_label():
+        def __init__(self, scale: float, text: str, x: float, y: float):
+            self.label = pyglet.text.Label(text,
+                                       font_name='Arial',
+                                       font_size=36*scale,
+                                       bold=True,
+                                       x=x, y=y,
+                                       anchor_x='center', anchor_y='center',
+                                       color=(0, 0, 0, 255))
+            randcolor = [random.randint(150,255), random.randint(150,255), random.randint(150,255)]
+            self.backdrop = pyglet.shapes.Circle(x=x, y=y, radius=100,
+                                                 color = (randcolor[0], randcolor[1], randcolor[2]))
+    def choose_num_players(self, welcome_message: str):
+        welcome_label = pyglet.text.Label(welcome_message,
+                                       font_name='Arial',
+                                       font_size=50,
+                                       bold=True,
+                                       x=self.window.width / 2, y=self.window.height * 3 / 4,
+                                       anchor_x='center', anchor_y='center',
+                                       color=(0, 0, 0, 255))
+        cpu_prompt_label = pyglet.text.Label('How many CPUs would you like to play against?',
+                                       font_name='Arial',
+                                       font_size=36,
+                                       x=self.window.width / 2, y=self.window.height * 2.5 / 4,
+                                       anchor_x='center', anchor_y='center',
+                                       color=(0, 0, 0, 255),)
+        self.labels.append(welcome_label)
+        self.labels.append(cpu_prompt_label)
+        x_inc = self.window.width/7
+        for i in range (1,7):
+            temp_click_label = self.clickable_circle_label(1.0, str(i), x_inc*i, self.window.height*1.5/4)
+            self.labels.append(temp_click_label.label)
+            self.shapes.append(temp_click_label.backdrop)
