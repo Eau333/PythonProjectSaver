fdc4283c808f517946bfe2094d84b0741a5fdb1e
Lucas Diaz Iwamoto <between630pm@gmail.com>
2024-02-10 17:30:46 -0500

    shoots ball with gravity

--- CannonGame/cannon_game_api.py
+++ CannonGame/cannon_game_api.py
@@ -2,6 +2,8 @@ import pyglet
 import ctypes
 import tkinter
 from tkinter import simpledialog
+import math
+import time
 ctypes.windll.user32.SetProcessDPIAware()
 
 
@@ -24,6 +26,7 @@ class CannonGameGfx:
                                                          width=150, height=150,
                                                          color=(0, 0, 0, 255),
                                                          border_color=(0, 0, 0, 255))
+        self.ball = None
         self.gunpowder = []
         self.root = tkinter.Tk()
         self.root.eval(f'tk::PlaceWindow {self.root._w} center')
@@ -36,6 +39,8 @@ class CannonGameGfx:
             self.grass.draw()
             self.playerone.draw()
             self.playertwo.draw()
+            if self.ball is not None:
+                self.ball.shape.draw()
         self.window.switch_to()
         self.window.dispatch_event('on_draw')
         self.window.dispatch_events()
@@ -50,3 +55,27 @@ class CannonGameGfx:
         selected_force = simpledialog.askstring(title="It's your turn.",
                                                      prompt=prompt,
                                                      parent=self.root)
+        self.ball = cannonball(float(selected_angle),float(selected_force))
+        self.fire()
+
+    def fire(self):
+        tick = 1/30
+        gravity = 300
+        for i in range(0, 1000):
+            self.update_window()
+            self.ball.shape.x += self.ball.xspeed * tick
+            self.ball.shape.y += self.ball.yspeed * tick
+            self.ball.yspeed -= gravity * tick
+            time.sleep(tick)
+
+class cannonball:
+    def __init__(self,angle,force):
+        self.shape = pyglet.shapes.Circle(x=350, y=350, radius=30,
+                                     color=(0, 0, 0))
+        self.cspeed = 3
+        self.rspeed = force * self.cspeed
+        self.xspeed = self.rspeed * math.cos(angle/180*math.pi)
+        self.yspeed = self.rspeed * math.sin(angle/180*math.pi)
+
+
+
